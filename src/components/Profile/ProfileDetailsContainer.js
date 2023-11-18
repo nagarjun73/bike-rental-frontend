56b52dea65d6276ab87ac722e34e7e4675c81133
nagarjun73 <nagarjun7397@gmail.com>
2023-11-18 19:56:03 +0530

    added profile page

---
+++ src/components/Profile/ProfileDetailsContainer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Stack, Card, CardContent, Typography, CardMedia, Box } from '@mui/material'
+
+export default function ProfileDetailsContainer(props) {
+  const { profile } = props
+
+  return (
+    <div>
+      <Card sx={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;", borderRadius: "35px" }}>
+        {
+          Object.keys(profile).length !== 0 && (<Box sx={{
+            display: 'flex', flexDirection: "column", justifyContent: 'center'
+          }
+          } >
+            < CardContent sx={{ paddingX: "8vw" }}>
+              <Stack sx={{ display: 'flex', flexDirection: "column" }}>
+                <Typography variant='h5'>UID: {profile._id}</Typography>
+                <Typography variant='p'>date created : {new Date(profile.createdAt).toLocaleString()}</Typography>
+              </Stack>
+            </CardContent >
+            <Typography variant='h5' margin="auto" paddingTop="5vh">Document ID</Typography>
+            <Stack direction="row" justifyContent="start">
+              {
+                profile.documentId.map((ele) => {
+                  return (< CardMedia
+                    component="img"
+                    alt={ele.url}
+                    height="200"
+                    width="300"
+                    key={ele._id}
+                    image={ele.url}
+                    sx={{ objectFit: "contain", }}
+                  />)
+                })
+              }
+            </Stack>
+
+            <Typography variant='h5' margin="auto" paddingTop="5vh">Driving Licence</Typography>
+            <Stack direction="row">
+              {
+                profile.drivingLicence.map((ele) => {
+                  return (< CardMedia
+                    component="img"
+                    alt={ele.url}
+                    height="200"
+                    key={ele._id}
+                    image={ele.url}
+                    sx={{ objectFit: "contain" }}
+                  />)
+                })
+              }
+            </Stack>
+          </Box >)
+        }
+      </Card>
+    </div>
+  )
+}
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  // baseURL: "http://localhost:3044"
-  baseURL: "https://bike-rental-backend.onrender.com"
+  baseURL: "http://localhost:3044"
+  // baseURL: "https://bike-rental-backend.onrender.com"
 })
