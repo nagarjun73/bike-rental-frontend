ca2259fd5a7cd7d659554632650e807a5b4b90cb
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2023-11-27 18:42:13 +0530

    added sort

--- src/components/HostVehicles/VehicleDetails/TripHistoryContainer.js
+++ src/components/HostVehicles/VehicleDetails/TripHistoryContainer.js
@@ -1,21 +1,41 @@
-import React from 'react'
-import { Card, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination, TableFooter } from '@mui/material'
+import React, { useEffect } from 'react'
+import { Card, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination, TableFooter, Box, FormControl, Select, MenuItem, FormLabel } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react'
 
 import TablePaginationActions from './TablePaginationActions'
 
 export default function TripHistoryContainer(props) {
-  const [page, setPage] = useState(0)
-  const [rowsPerPage, setRowsPerPage] = useState(5)
   const { vehicle } = props
+  const [page, setPage] = useState(0)
+  const [sort, setSort] = useState(-1)
+  const [filtered, setFilterd] = useState([])
+  const [rowsPerPage, setRowsPerPage] = useState(5)
 
-  console.log(vehicle, "trip history");
+  console.log(filtered, "state updateing");
+
+  useEffect(() => {
+    if (sort === -1) {
+      const vehicleFilterd = vehicle.sort((a, b) => {
+        return new Date(b.tripStartDate) - new Date(a.tripStartDate)
+      })
+      console.log(vehicleFilterd, "recent");
+      setFilterd(vehicleFilterd)
+    } else if (sort === 1) {
+      const vehicleFilterd = vehicle.sort((a, b) => {
+        return new Date(a.tripStartDate) - new Date(b.tripStartDate)
+      })
+      console.log(vehicleFilterd, "oldest");
+      setFilterd(vehicleFilterd)
+    }
+  }, [sort])
+
+
   const navigate = useNavigate()
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - vehicle.length) : 0;
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filtered.length) : 0;
 
   const handleTripDetails = (id) => {
     navigate(`/hosttripdetails/${id}`)
@@ -36,6 +56,24 @@ export default function TripHistoryContainer(props) {
       boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;",
       borderRadius: "15px"
     }}>
+
+      {/* Sorting */}
+      <Box marginLeft="2vw" >
+        <FormControl variant="standard" >
+          <Select
+            id="demo-simple-select"
+            value={sort}
+            onChange={(e) => setSort(e.target.value)}
+          >
+            <MenuItem value={-1}>Oldest First</MenuItem>
+            <MenuItem value={1}>Recent First</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
+
+
+      {/* Table */}
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -49,8 +87,8 @@ export default function TripHistoryContainer(props) {
           </TableHead>
           <TableBody>
             {(rowsPerPage > 0
-              ? vehicle.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : vehicle
+              ? filtered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : filtered
             ).map((vcl) => (
               <TableRow
                 key={vcl._id}
@@ -78,7 +116,7 @@ export default function TripHistoryContainer(props) {
               <TablePagination
                 rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                 colSpan={3}
-                count={vehicle.length}
+                count={filtered.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 SelectProps={{
